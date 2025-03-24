cbf3b91a27eaf22dcb6eb2421bf9a6e04545c368
lucapf <lucapf@gmail.com>
2025-03-24 10:06:45 +0400

    final commit, improve code coverage

--- src/test/java/org/pancakelab/model/OrderTest.java
+++ src/test/java/org/pancakelab/model/OrderTest.java
@@ -11,8 +11,11 @@ public class OrderTest {
         var order1 = new Order.Builder(1, 1).build();
         var order2 = new Order.Builder(order1).build();
         var order3 = new Order.Builder(1, 1).build();
+        assertEquals(order1, order1);
         assertEquals(order1, order2);
+        assertEquals(order1.hashCode(), order2.hashCode());
         assertNotEquals(order1, order3);
+        assertNotEquals(order1.hashCode(), order3.hashCode());
         assertNotEquals(order1, Order.Builder.buildNull("invalid"));
     }
 }
--- src/test/java/org/pancakelab/model/PancakeTest.java
+++ src/test/java/org/pancakelab/model/PancakeTest.java
@@ -2,8 +2,7 @@ package org.pancakelab.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PancakeTest {
     @Test
@@ -12,6 +11,8 @@ public class PancakeTest {
         var pancake2 = new Pancake.Builder(Ingredient.HAZELNUTS).build();
         var pancake3 = new Pancake.Builder(Ingredient.DARK_CHOCOLATE).build();
         assertEquals(pancake1, pancake2);
+        assertEquals(pancake1.hashCode(), pancake2.hashCode());
         assertNotEquals(pancake1, pancake3);
+        assertNotEquals(pancake1.hashCode(), pancake3.hashCode());
     }
 }
